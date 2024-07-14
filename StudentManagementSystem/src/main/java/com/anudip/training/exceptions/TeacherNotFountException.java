301bb104a260ea0f54ca947a48fb58f58b26797c
Suraj Bhalerao <154430492+SurajBhalerao27@users.noreply.github.com>
2024-07-14 12:19:16 +0530

    A student management system is done and commited

---
+++ StudentManagementSystem/src/main/java/com/anudip/training/exceptions/TeacherNotFountException.java
@@ -0,0 +1,12 @@
+package com.anudip.training.exceptions;
+
+public class TeacherNotFountException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TeacherNotFountException(String message) {
+		super(message);
+	}
+}
