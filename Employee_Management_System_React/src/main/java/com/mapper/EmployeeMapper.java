a43f4e6329fa945410b7612bbd5dd2ec98ac9955
Suraj Bhalerao <154430492+SurajBhalerao27@users.noreply.github.com>
2024-09-15 18:16:15 +0530

    Update EmployeeMapper.java

--- Employee_Management_System_React/src/main/java/com/mapper/EmployeeMapper.java
+++ Employee_Management_System_React/src/main/java/com/mapper/EmployeeMapper.java
@@ -4,7 +4,7 @@ import com.ems.dto.EmployeeDto;
 import com.ems.model.Employee;
 
 public class EmployeeMapper {
-	
+	// function for convert employeeDTO to employee 
 	public static EmployeeDto mapToEmployeeDto(Employee employee) {
 		return new EmployeeDto(
 				employee.getId(),
@@ -13,7 +13,9 @@ public class EmployeeMapper {
 				employee.getEmail()
 		);
 	}
-	
+
+
+	// function for convert employee to employeeDTO
 	public static Employee mapToEmployee(EmployeeDto employeeDto) {
 		
 		return new Employee(
